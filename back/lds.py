f3b6e957820cd7f0a7524cb8b69a904d5dc0b90c
Ethan Smith <ethan@esmithy.net>
2017-08-08 20:28:37 -0600

    Added functionality to service config and login functions

--- back/lds.py
+++ back/lds.py
@@ -1,3 +1,5 @@
+import os
+import requests
 
 
 class AuthenticationError(Exception):
@@ -8,18 +10,20 @@ _service_config = None
 
 
 def service_config():
+
     """Get the web service configuration.
 
     :return: deserialized JSON service configuration.
     """
     global _service_config
     if not _service_config:
-        pass
-        # fetch service config from https://tech.lds.org/mobile/ldstools/config.json
+        r = requests.get('https://tech.lds.org/mobile/ldstools/config.json')
+        r.raise_for_status()
+        _service_config = r.json()
     return _service_config
 
 
-def login(username, password):
+def login(username=None, password=None):
     """Log in with an LDS Account.
 
     :param: username, or None to get from an LDS_USERNAME environment variable.
@@ -27,6 +31,16 @@ def login(username, password):
     :return: a "requests" session with login cookies set.
     :raise: AuthenticationError if the credentials are incorrect.
     """
+    username = username or os.environ.get('LDS_USERNAME')
+    password = password or os.environ.get('LDS_PASSWORD')
+
+    s = requests.Session()
+    url = service_config()['auth-url']
+    data = {'username': username, 'password': password}
+    r = s.post(url, data)
+    if r.status_code != 200:
+        raise AuthenticationError(r)
+    return s
 
 
 def fetch_current_user_unit(s):
@@ -44,3 +58,7 @@ def fetch_callings(s, unit):
     :param s: an authenticated "requests" session.
     :return: deserialized JSON callings data.
     """
+
+
+if __name__ == '__main__':
+    login()
