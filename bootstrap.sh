c079996919ee185dbae2a0b6e59f6ae467126d27
Eric Smith <eric@esmithy.net>
2017-08-12 09:09:54 -0600

    Avoid conflict with Heroku's expected environment file. Remove pkg-resources.

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,5 +7,5 @@ curl -L https://cli-assets.heroku.com/apt/release.key | apt-key add -
 apt-get update
 
 apt-get install -y python3.6 python3.6-venv heroku redis-server
-python3.6 -m venv .env
+python3.6 -m venv .venv
 .env/bin/pip install -r requirements.txt
