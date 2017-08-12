46108afa095e7e471757e3a89e9b124f25b0d9c5
Eric Smith <eric@esmithy.net>
2017-08-12 15:00:15 -0600

    Initial database setup.

--- back/app.py
+++ back/app.py
@@ -1,13 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 from back.log import setup_logging, banner
 
 app = Flask(__name__)
+app.config.from_object('back.config')
 setup_logging(app.logger)
 banner(app.logger, ' web ')
+db = SQLAlchemy(app)
 
 # Imports to ensure the web application is set up:
 import back.views
+import back.models
 
 
 # Debugging entry point -- in production we run through gunicorn.
--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,11 @@ add-apt-repository "deb https://cli-assets.heroku.com/branches/stable/apt ./"
 curl -L https://cli-assets.heroku.com/apt/release.key | apt-key add -
 
 apt-get update
+apt-get install -y python3.6 python3.6-venv heroku redis-server postgresql
 
-apt-get install -y python3.6 python3.6-venv heroku redis-server
 python3.6 -m venv .venv
-.env/bin/pip install -r requirements.txt
+.venv/bin/pip install -r requirements.txt
+
+# Can I do this here?
+sudo -u postgres createuser -s $(whoami); createdb lds-callings
+# DATABASE_URL=postgres:///lds-callings
